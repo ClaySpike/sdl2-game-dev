3d259b59cd643aa7cbd083837293ac0df16f318d
ClaySpike <povinellimax@gmail.com>
2021-11-15 11:42:49 -0500

    Included main.c and global.h

---
+++ src/include/global.h
@@ -0,0 +1,6 @@
+#ifndef GLOBAL_FILE_H
+#define GLOBAL_FILE_H
+
+#define OK 0
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <include/global.h>
 
 int main(int argc, char *argv[]) {
-	printf("Hello Game Dev\n");
+	printf("Hello World!\n");
 	return 0;
 }
